b0679799cb77235248bc103938166b9252f8afca
Hemalatha-77GK <hemagkannan7@gmail.com>
2025-03-25 03:54:34 +0530

    added search function

--- product.js
+++ product.js
@@ -7,3 +7,20 @@ menuicon.addEventListener("click",function(){
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
 })
+var productlist=document.getElementById("productlist")
+var searchlist=document.getElementById("searchlist")
+var search=searchlist.querySelectorAll("div")
+
+productlist.addEventListener("keyup",function(){
+    var enteredvalue=event.target.value.toUpperCase()
+    for(count=0;count<search.length;count=count+1)
+    {
+        if(search[count].textContent.toUpperCase().indexOf(enteredvalue)<0)
+        {
+            search[count].style.display="none"
+        }
+        else{
+            search[count].style.display="block"
+        }
+    }
+})
